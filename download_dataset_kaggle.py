cd7fcae6ecd81e18f6138320d3c7515e38268a87
vasilesirbu95 <120591366+vasilesirbu95@users.noreply.github.com>
2025-03-08 20:04:43 +0100

    ...

--- download_dataset_kaggle.py
+++ download_dataset_kaggle.py
@@ -2,7 +2,7 @@ import kaggle
 
 kaggle.api.authenticate()
 
-print(kaggle.api.dataset_list_files('asinow/car-price-dataset').files)
+#print(kaggle.api.dataset_list_files('asinow/car-price-dataset').files)
 
 kaggle.api.dataset_download_files('asinow/car-price-dataset', path = '.', unzip = True)
 
